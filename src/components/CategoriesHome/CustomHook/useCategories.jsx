e940883cd14965905cc6a17a0e4c409c5d84336c
Lucas Correa <correalucas1905@gmail.com>
2023-10-18 15:33:19 -0300

    fix(useCategories): icon imports

--- src/components/CategoriesHome/CustomHook/useCategories.jsx
+++ src/components/CategoriesHome/CustomHook/useCategories.jsx
@@ -1,13 +1,13 @@
-import { PiDressFill, PiTelevisionSimpleBold } from "react-icons/Pi";
+import {  MdPhonelink } from "react-icons/md";
 import { FaTshirt } from "react-icons/fa";
-import { GiPearlNecklace } from "react-icons/gi";
+import { GiPearlNecklace,GiLargeDress } from "react-icons/gi";
 const useCategories = () => {
   const CATEGORIES = [
     { title: "Men's clothing", logo: <FaTshirt className="text-4xl" /> },
-    { title: "Women's clothing", logo: <PiDressFill className="text-4xl" /> },
+    { title: "Women's clothing", logo: <GiLargeDress className="text-4xl" /> },
     {
       title: "Electronics",
-      logo: <PiTelevisionSimpleBold className="text-4xl" />,
+      logo: <MdPhonelink className="text-4xl" />,
     },
     { title: "Jewelry", logo: <GiPearlNecklace className="text-4xl" /> },
   ];
