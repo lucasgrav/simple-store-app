082124c8c92680b53fc3ce3fd59adc2577380ff2
Lucas Correa <correalucas1905@gmail.com>
2023-10-19 15:56:47 -0300

    feat(home): edit the rounded of categories cards

--- src/components/CategoriesHome/CardCategory/CardCategory.jsx
+++ src/components/CategoriesHome/CardCategory/CardCategory.jsx
@@ -1,6 +1,6 @@
 const CardCategory = ({ children }) => {
   return (
-    <article className="h-36 text-center w-[150px] shadow-[1px_0px_7px_0px_#0000002b]  bg-[#ff4495] rounded-lg flex items-center justify-center font-Nunito font-bold text-white lg:h-56 lg:w-56 transition ease-in-out duration-300 lg:hover:scale-105 cursor-pointer">
+    <article className="h-36 text-center w-[150px] shadow-[1px_0px_7px_0px_#0000002b]  bg-[#ff4495] lg:hover:bg-[#ff6d6d] rounded-full p-4 flex items-center justify-center font-Nunito font-bold text-white lg:h-56 lg:w-56 transition ease-in-out duration-300 lg:hover:scale-105 cursor-pointer">
       {children}
     </article>
   );
