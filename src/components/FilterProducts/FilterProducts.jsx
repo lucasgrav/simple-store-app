fad5c18c431caa978fbbee5dcf790ff527d194fe
Lucas Correa <correalucas1905@gmail.com>
2023-10-20 15:28:40 -0300

    feat(product): input search

--- src/components/FilterProducts/FilterProducts.jsx
+++ src/components/FilterProducts/FilterProducts.jsx
@@ -1,11 +1,15 @@
+import InputSearch from "./Filters/InputSearch";
 import OptionPrice from "./Filters/OptionPrice";
 import OptionProducts from "./Filters/OptionProducts";
 
 const FilterProducts = () => {
   return (
-    <div className="flex justify-center mt-4 gap-4">
-      <OptionProducts />
-      <OptionPrice />
+    <div className="flex flex-col items-center justify-center mt-4 gap-4 md:flex-row md:gap-2">
+      <InputSearch />
+      <div className="flex gap-2">
+        <OptionProducts />
+        <OptionPrice />
+      </div>
     </div>
   );
 };
