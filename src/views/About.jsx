4f1bb8f4effe359fc036430965b9f3df19774011
Lucas Correa <correalucas1905@gmail.com>
2023-10-25 11:52:54 -0300

    feat(about): add animation

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,6 +1,19 @@
 import DesciptionAbout from "../components/About/DescriptionAbout";
+import { motion } from "framer-motion";
 
 const About = () => {
-  return <DesciptionAbout />;
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 1,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
+    >
+      <DesciptionAbout />
+    </motion.div>
+  );
 };
 export default About;
