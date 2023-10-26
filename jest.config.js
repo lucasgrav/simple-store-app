a89f51c5e4f35a6e90186d7c4c8e174fbb333caa
Lucas Correa <correalucas1905@gmail.com>
2023-10-26 11:12:59 -0300

    feat(vitest): vitest

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    testEnvironment: 'jsdom',
-    testMatch: [
-      '**/?(*.)+(spec|test|tests).[tj]s?(x)',
-    ],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  };
--- src/components/About/AboutSimple/AboutSimple.test.jsx
+++ src/components/About/AboutSimple/AboutSimple.test.jsx
@@ -1,10 +1,27 @@
-import { render } from "@testing-library/react";
-
+import { render, screen, cleanup } from "@testing-library/react";
+import { describe, it, afterEach,   expect } from "vitest";
 import AboutSimple from "./AboutSimple";
 
 describe("About Simple Description", () => {
-  it("Should render correctly", () => {
+  afterEach(cleanup);
+
+  it("Should render the component correctly", () => {
     render(<AboutSimple />);
-    
+
   });
+  it("Should render the title correctly", () => {
+    render(<AboutSimple />);
+    screen.getByText("SIMPLE-STORE");
+    screen.getByText("PROJECT");
+  });
+
+     it("Should render the GitHub link correctly", () => {
+    render(<AboutSimple />);
+    const githubLink = screen.getByRole("link");
+    expect(githubLink).toBeTruthy()
+    expect(githubLink).toHaveProperty(
+      "href",
+      "https://github.com/lucasgrav/simple-store-app"
+    );
+  }); 
 });
