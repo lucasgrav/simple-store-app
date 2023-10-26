a2c2e1c6015d9c302e7396443423060934e69ee0
Lucas Correa <correalucas1905@gmail.com>
2023-10-26 11:51:11 -0300

    feat(about/test): about simple description test

--- src/components/About/AboutSimple/AboutSimple.test.jsx
+++ src/components/About/AboutSimple/AboutSimple.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen, cleanup } from "@testing-library/react";
-import { describe, it, afterEach,   expect } from "vitest";
+import { describe, it, afterEach, expect } from "vitest";
 import AboutSimple from "./AboutSimple";
 
 describe("About Simple Description", () => {
@@ -7,21 +7,50 @@ describe("About Simple Description", () => {
 
   it("Should render the component correctly", () => {
     render(<AboutSimple />);
-
   });
+
   it("Should render the title correctly", () => {
     render(<AboutSimple />);
-    screen.getByText("SIMPLE-STORE");
-    screen.getByText("PROJECT");
+    const title = screen.getByRole("heading", { name: "SIMPLE-STORE PROJECT" });
+    expect(title).toBeTruthy();
   });
 
-     it("Should render the GitHub link correctly", () => {
+  it("Should render the image correctly", () => {
+    render(<AboutSimple />);
+    const imageGirl = screen.getByRole("img", { name: "Girl jumping" });
+    expect(imageGirl).toBeTruthy();
+    expect(imageGirl).toHaveProperty(
+      "src",
+      "/assets/images/photo-girl-like.jpg"
+    );
+  });
+
+  it("Should render the paragraph correctly", () => {
+    render(<AboutSimple />);
+
+    const paragraph = screen.getByText(
+      /is a fake e-commerce website that fetches all its information from the/i
+    );
+
+    // Ahora puedes realizar tus aserciones en el párrafo.
+    expect(paragraph).toBeTruthy();
+  });
+  it("Should render the GitHub link correctly", () => {
     render(<AboutSimple />);
     const githubLink = screen.getByRole("link");
-    expect(githubLink).toBeTruthy()
+    expect(githubLink).toBeTruthy();
     expect(githubLink).toHaveProperty(
       "href",
       "https://github.com/lucasgrav/simple-store-app"
     );
-  }); 
+  });
+
+  it("Should render the GitHub SVG correctly", () => {
+    render(<AboutSimple />);
+    const githubLink = screen.getByRole("link", {
+      href: "https://github.com/lucasgrav/simple-store-app",
+    });
+    const githubSVG = githubLink.querySelector("svg");
+    expect(githubSVG).toBeTruthy();
+  });
 });
