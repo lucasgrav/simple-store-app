5deebe61fea50b207576c14fecde16229792a93e
Lucas Correa <correalucas1905@gmail.com>
2023-10-31 10:07:13 -0300

    feat(containerProducts): testing

---
+++ src/components/ContainerProducts/ContainerProducts.test.jsx
@@ -0,0 +1,19 @@
+import { render, cleanup } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { describe, it, afterEach } from "vitest";
+import store from "../../redux/store";
+import { BrowserRouter } from "react-router-dom";
+import ContainerProducts from "./ContainerProducts";
+
+describe("Container products", () => {
+  afterEach(cleanup);
+  it("Should render the component correctly", () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <ContainerProducts />
+        </BrowserRouter>
+      </Provider>
+    );
+  });
+});
